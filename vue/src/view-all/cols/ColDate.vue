822b0f90a48d00f6cbf3419f284c7740bc517755
smdyachenko@gmail.com <smdyachenko@gmail.com>
2022-12-02 09:10:50 +0200

    Date works good

--- vue/src/view-all/cols/ColDate.vue
+++ vue/src/view-all/cols/ColDate.vue
@@ -1,7 +1,12 @@
 <template>
-   <span>{{data}}</span>
+   <span>{{showDate(data)}}</span>
 </template>
 
 <script setup>
 let props = defineProps({data: Date})
+
+/** @param {Date} data */
+function showDate(date){
+   return new Intl.DateTimeFormat("ru", { dateStyle: 'short', timeStyle: 'short' }).format(date)
+}
 </script>
