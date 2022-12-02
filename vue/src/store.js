a6499acd3cca266be81bb48109886735ad7d90bd
smdyachenko@gmail.com <smdyachenko@gmail.com>
2022-12-02 07:41:35 +0200

    Awoid multiple requests

--- vue/src/store.js
+++ vue/src/store.js
@@ -8,12 +8,16 @@ export const spBeans = reactive({})
 export async function FillColsData(schema, table, refresh = false){
    let key = spTableKey(schema, table)
    if (spColsData[key] && !refresh) return;
+   if (spColsData[key] === 'loading') return
+   spColsData[key] = 'loading'
    spColsData[key] = await api[schema][table].GetColsData()
 }
 
 export async function FillBeans(schema, table, refresh = false){
    let key = spTableKey(schema, table)
    if (spBeans[key] && !refresh) return;
+   if (spBeans[key] === 'loading') return
+   spBeans[key] = 'loading'
    spBeans[key] = await api[schema][table].GetBeans()
 }
 
--- vue/src/view-all/cols/ColFk.vue
+++ vue/src/view-all/cols/ColFk.vue
@@ -6,24 +6,25 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import { spBeans, FillBeans,  spTableKey } from '../../store'
 import { Col } from 'common/col'
+import { computed } from '@vue/reactivity';
 
 export default defineComponent({
    props: ['col', 'bean'],
 
    /** @param {{col: Col, bean: any}} props */
    setup(props) {
-      let data = ref('')
       function init() {
-         FillBeans(props.col.fk.foreign_table_schema, props.col.fk.foreign_table_name).then(() => {
-            data.value = findData()
-         })
+         FillBeans(props.col.fk.foreign_table_schema, props.col.fk.foreign_table_name)
       }
       onMounted(init)
-
+      let data = computed(() => {
+         return findData()
+      })
       function findData() {
          let { fk } = props.col
          let id = props.bean[props.col.column_name]
          let tableKey = spTableKey(fk.foreign_table_schema, fk.foreign_table_name)
+         if (!Array.isArray(spBeans[tableKey])) return props.bean[props.col.column_name]
          let fkBean = spBeans[tableKey].find(el => el[fk.foreign_column_name] === id)
          if (fkBean) return fkBean[fk.foreign_title_column_name]
          return props.bean[props.col.column_name]
